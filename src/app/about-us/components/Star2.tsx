66ac8c5a48cea57723417141d1679c6ea201273a
fnathas <farrel.saskoro@gmail.com>
2025-07-19 17:47:04 +0700

    feat: add about us page

---
+++ src/app/about-us/components/Star2.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+const Star2 = () => (
+    <Image
+        src="/star-au-2.png"
+        alt="Star Decoration"
+        width={1000}
+        height={300}
+        style={{ width: '100%', height: 'auto' }}
+    />
+);
+
+export default Star2;
